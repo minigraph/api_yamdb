692d37540b311ed2f020cf777662681ba3b9cf58
Nikitin Michael <maikl.nikitin@yahoo.com>
2022-12-01 17:02:40 +0300

    create comment models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class Category(models.Model):
@@ -121,3 +122,37 @@ class GenresOfTitles(models.Model):
 
     def __str__(self):
         return f':Жанр {self.genre} произведения {self.title}'
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    text = models.TextField(
+        'Текст',
+        help_text='Текст комментария'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+        help_text='Отзыв',
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
