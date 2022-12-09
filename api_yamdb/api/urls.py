95fefbcc395967346cb64608c5abc0a46fd420b9
Nikitin Michael <maikl.nikitin@yahoo.com>
2022-12-09 15:47:04 +0300

    API v1 перемещено в отдельную папку, изменены пути

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,28 +1,6 @@
 from django.urls import include, path
-from rest_framework import routers
-
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet, UserViewSet, get_jwt_token,
-                    send_confirmation_code)
-
-router_v1 = routers.DefaultRouter()
-router_v1.register(r'users', UserViewSet)
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='reviews'
-)
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
-router_v1.register('categories', CategoryViewSet)
-router_v1.register('genres', GenreViewSet)
-router_v1.register('titles', TitleViewSet)
+from .v1 import urls as v1_urls
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/auth/signup/', send_confirmation_code),
-    path('v1/auth/token/', get_jwt_token)
+    path('v1/', include(v1_urls)),
 ]
